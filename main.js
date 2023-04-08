6ba08a4feeb0cd6fec537a6eaef065aeea3929dd
Yarvis405 <115061721+Yarvis405@users.noreply.github.com>
2023-04-08 00:53:47 -0700

    Initial Commit

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 "use strict";
 import players from 'data.json' assert{type: 'json'}
 
-const view = document.querySelector("#app").innerHTML = () => {
-    return(`
-        <h2>players are equal to:</h2>
-        ${players}
-    `)
-}
+const view = document.querySelector("#app").innerHTML = players;
