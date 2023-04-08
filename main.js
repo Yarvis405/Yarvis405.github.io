a87654dea70dd48c8dd1fdb7ce2619a1a6f3f7de
Yarvis405 <115061721+Yarvis405@users.noreply.github.com>
2023-04-08 02:41:39 -0700

    Initial Commit

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ import players from './data.json' assert{type: 'json'}
 
 alert(stringify.JSON(players));
 
-const view = document.getElementById("#app").innerHTML = stringify.JSON(players);
+const view = document.getElementById("#app").innerText = stringify.JSON(players);
