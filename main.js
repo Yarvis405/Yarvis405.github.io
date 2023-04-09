356d21a1db12f3f02b807dd54ae95f693e693e8b
Yarvis405 <115061721+Yarvis405@users.noreply.github.com>
2023-04-08 19:08:29 -0700

    Initial Commit

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const app = document.querySelector("#app")
 
 const getData = async () => {
-    data = await fetch('./data.json');
-    res = await data.json();
+    let data = await fetch('./data.json');
+    let res = await data.json();
     
     app.innerText = JSON.stringify(res);
 }
