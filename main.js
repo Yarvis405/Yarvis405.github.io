3b7f660de12692cfda3ac514d203dff8f63e996e
Yarvis405 <115061721+Yarvis405@users.noreply.github.com>
2023-04-08 22:36:16 -0700

    Initial Commit

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const app = document.querySelector("#app")
 const jsonData = "https://jsonplaceholder.typicode.com/users"
 
-const getData = async () => {
-    let data = await fetch(jsonData);
+const getData = async (file) => {
+    let data = await fetch(file);
     let res = await data.json();
     
     for (i in res.players){
-        app.innerText = res.players[i]
+        app.innerText = res[i]
     }
 }
 
-getData();
+getData(jsonData);
 
